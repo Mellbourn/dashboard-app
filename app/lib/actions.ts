0b88f2b8ac7378094bfe6c902a1db8aca44a5ca9
Klas Mellbourn <klas@mellbourn.net>
2024-01-01 14:24:20 +0100

    edit form validated too

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -71,12 +71,26 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 // ...
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  prevState: State,
+  id: string,
+  formData: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  // Prepare data for updating the database
+  const { customerId, amount, status } = validatedFields.data;
 
   const amountInCents = amount * 100;
 
